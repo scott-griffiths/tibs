c2921528c5d500abf183a6baee939a8d42030e06
Scott Griffiths <dr.scottgriffiths@gmail.com>
2026-05-30 09:06:07 +0100

    Improving Dtype construction and display.

--- src/dtype.rs
+++ src/dtype.rs
@@ -20,10 +20,41 @@ impl Dtype {
                 length
             )));
         }
+        let length = length as usize;
+        let byte_order = byte_order.unwrap_or(Endianness::Unspecified);
+        match byte_order {
+            Endianness::Unspecified => (),
+            _ => {
+                match kind {
+                    DtypeKind::Bin | DtypeKind::Hex | DtypeKind::Oct | DtypeKind::Bytes => {
+                        return Err(PyValueError::new_err(format!(
+                            "A byte order cannot be specified for a Dtype of type {}.", kind.repr_name()
+                        )))
+                    },
+                    _ => {
+                        if !length.is_multiple_of(8) {
+                            return Err(PyValueError::new_err(format!(
+                                "If a Dtype byte_order is given, the length must be a multiple of 8 (length = {}).", length
+                            )))
+                        }
+                    }
+                }
+            }
+        }
+        if byte_order != Endianness::Unspecified {
+            match kind {
+                DtypeKind::Bin | DtypeKind::Hex | DtypeKind::Oct | DtypeKind::Bytes => {
+                    return Err(PyValueError::new_err(format!(
+                        "A byte order cannot be specified for a Dtype of type {}.", kind.repr_name()
+                    )))
+                },
+                _ => ()
+            }
+        }
         Ok(Dtype {
             kind,
-            length: length as usize,
-            byte_order: byte_order.unwrap_or(Endianness::Unspecified),
+            length,
+            byte_order
         })
     }
 
@@ -58,43 +89,39 @@ impl Dtype {
     }
 
     #[classmethod]
-    #[pyo3(signature = (length, byte_order = Endianness::Unspecified), text_signature = "(cls, length, byte_order)")]
+    #[pyo3(signature = (length), text_signature = "(cls, length)")]
     pub fn bytes(
         _cls: &pyo3::Bound<'_, pyo3::types::PyType>,
         length: i64,
-        byte_order: Option<Endianness>,
     ) -> PyResult<Self> {
-        Self::py_new(DtypeKind::Bytes, length, byte_order)
+        Self::py_new(DtypeKind::Bytes, length, None)
     }
 
     #[classmethod]
-    #[pyo3(signature = (length, byte_order = Endianness::Unspecified), text_signature = "(cls, length, byte_order)")]
+    #[pyo3(signature = (length), text_signature = "(cls, length)")]
     pub fn bin(
         _cls: &pyo3::Bound<'_, pyo3::types::PyType>,
         length: i64,
-        byte_order: Option<Endianness>,
     ) -> PyResult<Self> {
-        Self::py_new(DtypeKind::Bin, length, byte_order)
+        Self::py_new(DtypeKind::Bin, length, None)
     }
 
     #[classmethod]
-    #[pyo3(signature = (length, byte_order = Endianness::Unspecified), text_signature = "(cls, length, byte_order)")]
+    #[pyo3(signature = (length), text_signature = "(cls, length)")]
     pub fn oct(
         _cls: &pyo3::Bound<'_, pyo3::types::PyType>,
         length: i64,
-        byte_order: Option<Endianness>,
     ) -> PyResult<Self> {
-        Self::py_new(DtypeKind::Oct, length, byte_order)
+        Self::py_new(DtypeKind::Oct, length, None)
     }
 
     #[classmethod]
-    #[pyo3(signature = (length, byte_order = Endianness::Unspecified), text_signature = "(cls, length, byte_order)")]
+    #[pyo3(signature = (length), text_signature = "(cls, length)")]
     pub fn hex(
         _cls: &pyo3::Bound<'_, pyo3::types::PyType>,
         length: i64,
-        byte_order: Option<Endianness>,
     ) -> PyResult<Self> {
-        Self::py_new(DtypeKind::Hex, length, byte_order)
+        Self::py_new(DtypeKind::Hex, length, None)
     }
 
     #[getter]
@@ -113,11 +140,32 @@ impl Dtype {
     }
 
     pub fn __repr__(&self) -> String {
-        format!(
-            "Dtype({}, {}, {})",
-            self.kind.repr_name(),
-            self.length,
-            self.byte_order.repr_name(),
-        )
+        let byte_order_str = match self.byte_order {
+            Endianness::Unspecified => "".to_string(),
+            _ => format!(", {}", self.byte_order.repr_name())
+        };
+        match self.kind {
+            DtypeKind::Uint => {
+                format!("Dtype.u({}{})", self.length, byte_order_str)
+            },
+            DtypeKind::Int => {
+                format!("Dtype.i({}{})", self.length, byte_order_str)
+            },
+            DtypeKind::Float => {
+                format!("Dtype.f({}{})", self.length, byte_order_str)
+            },
+            DtypeKind::Bin => {
+                format!("Dtype.bin({})", self.length)
+            },
+            DtypeKind::Oct => {
+                format!("Dtype.oct({})", self.length)
+            },
+            DtypeKind::Hex => {
+                format!("Dtype.hex({})", self.length)
+            },
+            DtypeKind::Bytes => {
+                format!("Dtype.bytes({})", self.length)
+            },
+        }
     }
 }
